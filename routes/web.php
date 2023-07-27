b2970425addb70958152ecd96c8e26b56e5216ab
sutartidwiyanti <201220085@unmuhpnk.ac.id>
2023-07-27 17:03:33 +0700

    memperbaiki fungsi login

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-});
+//Route::get('/login', function () {
+//    return view('login');
+//});
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login/user', [LoginController::class, 'authenticate']);
 
 Route::get('/register', function () {
     return view('register');
