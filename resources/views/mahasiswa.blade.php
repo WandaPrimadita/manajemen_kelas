9eebad8fc00b554c7cdb9b46f3b4e788696e34e3
sutartidwiyanti <201220085@unmuhpnk.ac.id>
2023-07-28 12:00:33 +0700

    menambahkan bagian footer

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -79,7 +79,7 @@
 </section>
 
     <!-- Footer-->
-    <footer class="py-5 bg-black">
+    <footer class="text-center py-1">
                 <div class="container px-5">
                 <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-success text-white">
                 <p class="m-0 text-center">Copyright &copy; Your Website 2023 (Manhasla)</p>
