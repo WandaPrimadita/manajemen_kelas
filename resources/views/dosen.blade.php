c02b69707964481807903ca3a64538e68e7bf8cb
enkanfenyy19 <enkanfeny309@gmail.com>
2023-07-28 16:31:40 +0700

    edit view

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -14,7 +14,7 @@
 <body>
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success text-white">
   		<div class="container">
-            <a class="navbar-brand" href="#">Manhasla</a>
+            <a class="navbar-brand" href="/">Manhasla</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
                 </button>
